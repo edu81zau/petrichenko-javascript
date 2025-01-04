735e254fd9848434fff67bacaf86ec18816ca26f
A.Zahliada <edu81zau@gmail.com>
2025-01-04 16:02:44 +0200

    slider-css: added to work with second buttons

--- sander/my-slider/sander-css/script.js
+++ sander/my-slider/sander-css/script.js
@@ -19,16 +19,24 @@ const app = {
 
             //---------- Display ----------
             this.buttonAS1 = document.querySelector("#demoDisplay .buttonAS1");
-            this.buttonAS1.addEventListener("click", this.slide1Activation.bind(this));
+            this.buttonAS1.addEventListener("click", this.onClickChangeSlide.bind(this));
             this.buttonAS2 = document.querySelector("#demoDisplay .buttonAS2");
-            this.buttonAS2.addEventListener("click", this.slide1Activation.bind(this));
+            this.buttonAS2.addEventListener("click", this.onClickChangeSlide.bind(this));
             this.sliderContainer = document.querySelector("#demoDisplay .slider-container");
             this.slides = this.sliderContainer.querySelectorAll(".slide");
         },
-        slide1Activation: function () {
-            console.log("app.demoDisplay.slide1Activation", arguments);
-            this.slides[0].classList.add("active");
-             //
+        onClickChangeSlide: function () {
+            console.log("app.demoDisplay.onClickChangeSlide", arguments);
+            if(arguments[0]["target"].className === "buttonAS1"){
+                this.slides[1].classList.remove("active");
+                this.slides[0].classList.add("active");
+                console.log(this.slides);
+            } else {
+                this.slides[0].classList.remove("active");
+                this.slides[1].style.backgroundColor = "forestgreen";
+                this.slides[1].classList.add("active");
+            }
+            //
         }
     },
     demoVisibility:{
@@ -44,15 +52,23 @@ const app = {
             console.log("app.demoVisibility.init", arguments);
             //---------- Visibility ----------
             this.buttonAS1 = document.querySelector("#demoVisibility .buttonAS1");
-            this.buttonAS1.addEventListener("click", this.slide1Activation.bind(this));
+            this.buttonAS1.addEventListener("click", this.onClickChangeSlide.bind(this));
             this.buttonAS2 = document.querySelector("#demoVisibility .buttonAS2");
-            this.buttonAS2.addEventListener("click", this.slide1Activation.bind(this));
+            this.buttonAS2.addEventListener("click", this.onClickChangeSlide.bind(this));
             this.sliderContainer = document.querySelector("#demoVisibility .slider-container");
             this.slides = this.sliderContainer.querySelectorAll(".slide");
         },
-        slide1Activation: function () {
-            console.log("app.demoVisibility.slide1Activation", arguments);
-            this.slides[0].classList.add("active");
+        onClickChangeSlide: function () {
+            console.log("app.demoVisibility.onClickChangeSlide", arguments);
+            if(arguments[0]["target"].className === "buttonAS1"){
+                this.slides[1].classList.remove("active");
+                this.slides[0].classList.add("active");
+                console.log(this.slides);
+            } else {
+                this.slides[0].classList.remove("active");
+                this.slides[1].style.backgroundColor = "gold";
+                this.slides[1].classList.add("active");
+            }
             //
         },
         //
@@ -70,15 +86,23 @@ const app = {
             console.log("app.demoOpacity.init", arguments);
             //---------- Opacity ----------
             this.buttonAS1 = document.querySelector("#demoOpacity .buttonAS1");
-            this.buttonAS1.addEventListener("click", this.slide1Activation.bind(this));
+            this.buttonAS1.addEventListener("click", this.onClickChangeSlide.bind(this));
             this.buttonAS2 = document.querySelector("#demoOpacity .buttonAS2");
-            this.buttonAS2.addEventListener("click", this.slide1Activation.bind(this));
+            this.buttonAS2.addEventListener("click", this.onClickChangeSlide.bind(this));
             this.sliderContainer = document.querySelector("#demoOpacity .slider-container");
             this.slides = this.sliderContainer.querySelectorAll(".slide");
         },
-        slide1Activation: function () {
-            console.log("app.demoOpacity.slide1Activation", arguments);
-            this.slides[0].classList.add("active");
+        onClickChangeSlide: function () {
+            console.log("app.demoOpacity.onClickChangeSlide", arguments);
+            if(arguments[0]["target"].className==="buttonAS1"){
+                this.slides[1].classList.remove("active");
+                this.slides[0].classList.add("active");
+                console.log(this.slides);
+            } else {
+                this.slides[0].classList.remove("active");
+                this.slides[1].style.backgroundColor = "darkorange";
+                this.slides[1].classList.add("active");
+            }
             //
         }
 
