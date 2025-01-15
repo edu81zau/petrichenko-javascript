fb10a9f02c5fc29dcf1fb015d0c38c57fbf4b143
A.Zahliada <edu81zau@gmail.com>
2025-01-15 13:42:47 +0200

    Slider: dynamically adding buttons

--- sander/my-slider/z-index-slider/public/js/Slider.js
+++ sander/my-slider/z-index-slider/public/js/Slider.js
@@ -4,7 +4,6 @@ class Slider {
     pointContainer;
     buttonNext;
     buttonPrev;
-    numberSlide;
     indexElement = 0;
     items;
     points;
@@ -13,21 +12,31 @@ class Slider {
         console.log("sliderSelector.constructor", sliderSelector);
         this.rootEl = document.querySelector(sliderSelector);
         console.log("rootEl", this.rootEl);
+        //@TODO проверить rootEl на null
         if (this.rootEl.length < 1) {
             throw new Error("Elements not found");
             return;
         }
 
+
+        this.sliderContainer = this.rootEl.querySelector(".zau-slider__items");
+        this.items = this.sliderContainer.querySelectorAll(".zau-slider__item");
+        this.pointContainer = this.rootEl.querySelector(".zau-slider__points");
+        this.points = [];
+        //@TODO: проверить, что все элементы найдены
+
         this.init();
     }
 
     init() {
+//@TODO: избавиться от всех if, которые проверены в конструкторе
         this.buttonNext = this.rootEl.querySelector(".next");
         if (this.buttonNext) {
-           this.buttonNext.addEventListener("click", this.onClickUp.bind(this));
+            this.buttonNext.addEventListener("click", this.onClickUp.bind(this));
         } else {
             throw new Error("Button not found");
         }
+
         this.buttonPrev = this.rootEl.querySelector(".prev");
         if (this.buttonPrev) {
             this.buttonPrev.addEventListener("click", this.onClickDown.bind(this));
@@ -35,10 +44,17 @@ class Slider {
             throw new Error("Button not found");
         }
 
-        this.sliderContainer = this.rootEl.querySelector(".zau-slider__items");
-        this.items = this.sliderContainer.querySelectorAll(".zau-slider__item");
-        this.pointContainer = this.rootEl.querySelector(".zau-slider__points");
-        this.points = this.pointContainer.querySelectorAll(".zau-slider__point");
+        const fragment = document.createDocumentFragment();
+        if (this.items.length > 0) {
+            this.items.forEach((item) => {
+                const newPoint = document.createElement("div");
+                newPoint.classList.add("zau-slider__point");
+                this.points.push(newPoint);
+                fragment.appendChild(newPoint);
+            })
+        }
+
+        this.pointContainer.appendChild(fragment);
         this.pointContainer.addEventListener("click", this.onClickChange.bind(this));
         this.setActiveSlide(this.indexElement);
     }
@@ -65,7 +81,7 @@ class Slider {
         this.setActiveSlide(newIndex);
     }
 
-    onClickDown(){
+    onClickDown() {
         console.log("Slider.onClickDown");
         let newIndex = this.indexElement - 1;
 
@@ -74,8 +90,11 @@ class Slider {
         }
         this.setActiveSlide(newIndex);
     }
+
     onClickChange(event) {
         console.log("Slider.onClickChange", arguments);
+        //@TODO: разобрать и разнести на несколько строк.
+        // Методу setActiveSlide должна передаваться переменная
         this.setActiveSlide(Array.from(event.target.parentNode.children).indexOf(event.target));
     }
 }
