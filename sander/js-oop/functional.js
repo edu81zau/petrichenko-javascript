8b0b39f2930ec1da67dd3e4aea9575ffdfe831cc
A.Zahliada <edu81zau@gmail.com>
2025-01-28 18:35:20 +0200

    class Animals rename to Animal

--- sander/js-oop/functional.js
+++ sander/js-oop/functional.js
@@ -1,35 +1,34 @@
-
-function Animals(alias) {
-    console.log('Animals.constructor', alias)
+function Animal(alias) {
+    console.log('Animal.constructor', alias)
     this.alias = alias;
     this.changeAlias = function (newAlias) {
-        console.log('Animals.changeAlias', newAlias)
+        console.log('Animal.changeAlias', newAlias)
         this.alias = newAlias;
     }
 }
 
-Animals.prototype = {
+Animal.prototype = {
     getSound: function () {
-        console.log('Animals.prototype.getSound');
+        console.log('Animal.prototype.getSound');
         throw new Error('Этот метод абстрактный. Следует переопределить его в потомках.');
     }
 }
 
-Animals.prototype.saySound = function () {
-    console.log('Animals.prototype.saySound ...');
+Animal.prototype.saySound = function () {
+    console.log('Animal.prototype.saySound ...');
     const
         sound = this.getSound(),
         res = this.alias + ' say ' + sound
     ;
-    console.log('Animals.prototype.saySound: ', res);
+    console.log('Animal.prototype.saySound: ', res);
 }
 
 function Dog(alias) {
     console.log('Dog.constructor');
-    Animals.call(this, alias);
+    Animal.call(this, alias);
 }
 
-Dog.prototype = Object.create(Animals.prototype);
+Dog.prototype = Object.create(Animal.prototype);
 Dog.prototype.constructor = Dog;
 Dog.prototype.getSound = function () {
     console.log('Dog.prototype.getSound');
@@ -39,9 +38,10 @@ Dog.prototype.getSound = function () {
 
 function Cat(alias) {
     console.log('Cat.constructor');
-    Animals.call(this, alias);
+    Animal.call(this, alias);
 }
-Cat.prototype = Object.create(Animals.prototype);
+
+Cat.prototype = Object.create(Animal.prototype);
 Cat.prototype.constructor = Cat;
 Cat.prototype.getSound = function () {
     console.log('Cat.prototype.getSound');
@@ -57,5 +57,5 @@ dog.saySound();
 let cat = new Cat("Murka");
 cat.saySound();
 
-let abstractAnimal = new Animals("Fish");
-abstractAnimal.saySound();
+// let abstractAnimal = new Animal("Fish");
+// abstractAnimal.saySound();
