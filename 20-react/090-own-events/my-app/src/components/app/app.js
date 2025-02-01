df71d7d9d2776ee27c121385eb091f380de370d8
A.Zahliada <edu81zau@gmail.com>
2025-02-01 18:51:31 +0200

    Did 134 lesson. Immutable state and own events

---
+++ 20-react/090-own-events/my-app/src/components/app/app.js
@@ -0,0 +1,88 @@
+import {Component} from "react";
+
+import AppInfo from '../app-info/app-info'
+import SearchPanel from '../search-panel/search-panel.js'
+import AppFilter from '../app-filter/app-filter'
+import EmployersList from '../employers-list/employers-list';
+import EmployersAddForm from "../employers-add-form/employers-add-form";
+
+import './app.css';
+
+class App extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: [
+                {name: 'John C.', salary: 800, increase: false, id: 1},
+                {name: 'Alex M.', salary: 3000, increase: true, id: 2},
+                {name: 'Carl D.', salary: 5000, increase: false, id: 3},
+            ],
+            userInfo:{
+                login:"User1",
+                password:"Password1",
+                email:"user1@email.com",
+            }
+        }
+        this.lastId = 4;
+    }
+
+    deleteItem = (id) => {
+        this.setState(
+            (
+            //    {data,userInfo}
+                state
+            ) => {
+                //console.log("Generate state for deleteItem",data,userInfo);
+                console.log("Generate state for deleteItem",state);
+                const data = state.data;
+                // const index = data.findIndex(elem => elem.id === id);
+                // const before = data.slice(0, index);
+                // const after = data.slice(index + 1);
+                // const newArray = [...before, ...after];
+
+                return {
+                    data: data.filter(item => item.id !== id)
+                }
+            }
+        )
+    }
+
+    addItem = (name, salary) => {
+        const newData = {
+            name,
+            salary,
+            increase: false,
+            id: this.lastId++,
+        }
+
+        this.setState(({data}) => {
+            const newArray = [...data, newData];
+            return {
+                data: newArray
+            }
+        })
+    }
+
+
+    render() {
+        return (
+            <div className="app">
+                <AppInfo/>
+                <div className="search-panel">
+                    <SearchPanel/>
+                    <AppFilter/>
+                </div>
+                <EmployersList
+                    data={this.state.data}
+                    onDelete={this.deleteItem}
+                />
+                <EmployersAddForm
+                    onAdd={this.addItem}
+                />
+            </div>
+        );
+    }
+}
+
+export default App;
---
+++ 20-react/090-own-events/my-app/src/components/employers-list-item/employers-list-item.js
@@ -0,0 +1,61 @@
+import {Component} from "react";
+
+import './employers-list-item.css';
+
+class EmployersListItem extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            increase: false,
+            star: false
+        }
+    }
+
+    onIncrease = () => {
+        this.setState(({increase}) => ({
+            increase: !increase
+        }))
+    }
+
+    onStar = () => {
+        this.setState(({star}) => ({
+            star: !star
+        }))
+    }
+
+    render() {
+        const {name, salary, onDelete} = this.props;
+        const {increase, star} = this.state;
+
+        let className = 'list-group-item d-flex justify-content-between';
+        if (increase) {
+            className += ' increase';
+        }
+        if (star) {
+            className += ' like';
+        }
+        return (
+            <li className={className}>
+                <span className="list-group-item-label"
+                      onClick={this.onStar}>{name}</span>
+                <input type="text" className="list-group-item-input" defaultValue={salary + "$"}/>
+                <div className='d-flex justify-content-center align-items-center'>
+                    <button type="button"
+                            className="btn-cookie btn-sm "
+                            onClick={this.onIncrease}>
+                        <i className="fas fa-cookie"></i>
+                    </button>
+
+                    <button type="button"
+                            className="btn-trash btn-sm "
+                            onClick={onDelete}>
+                        <i className="fas fa-trash"></i>
+                    </button>
+                    <i className="fas fa-star"></i>
+                </div>
+            </li>
+        );
+    }
+}
+
+export default EmployersListItem;
