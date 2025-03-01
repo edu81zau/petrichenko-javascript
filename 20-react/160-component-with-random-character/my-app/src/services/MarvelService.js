d796cf5cab8109f99132726b4e2129123b7f9bde
A.Zahliada <edu81zau@gmail.com>
2025-03-01 16:25:52 +0200

    Did homework

--- 20-react/160-component-with-random-character/my-app/src/services/MarvelService.js
+++ 20-react/160-component-with-random-character/my-app/src/services/MarvelService.js
@@ -12,22 +12,33 @@ class MarvelService {
     }
 
     getAllCharacters = async () => {
-        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
-        return res.data.results.map(this._transformCharacter) ;
+        let res = await this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+        res = res.data.results.map(this._transformCharacter);
+        return res;
     }
     getCharacter = async (id) => {
-        const res = await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
-        return this._transformCharacter(res.data.results[0]);
+        let res = await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+        res = this._transformCharacter(res.data.results[0]);
+        if (res.description === "") {
+            res.description = "Sorry, but we didn't find description";
+        }
+        if ( res.description.length > 146){
+            res.description = res.description.slice(0, 146) + " ... to continue go on homepage ";
+        }
+
+
+        return res;
     }
 
     _transformCharacter = (char) => {
-        return {
+        const res = {
             name: char.name,
             description: char.description,
             thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
             homepage: char.urls[0].url,
             wiki: char.urls[1].url,
         }
+        return res;
     }
 }
 
