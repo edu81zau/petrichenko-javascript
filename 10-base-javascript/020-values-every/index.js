52d6e8514a27452a6bdbb0559b892e0f31dec79e
A.Zahliada <edu81zau@gmail.com>
2025-04-20 16:27:15 +0300

    The text was formatted

--- 10-base-javascript/020-values-every/index.js
+++ 10-base-javascript/020-values-every/index.js
@@ -1,15 +1,15 @@
 (function () {
-  /*
-    const form = document.getElementById("myForm");
-    const labels = form.querySelectorAll("label");
-    */
-  var form = document.getElementById("myForm");
-  var labels =
-    form === null || form === void 0 ? void 0 : form.querySelectorAll("label");
-  form.addEventListener("submit", function (e) {
-    e.preventDefault();
-    var newLabel = document.createElement("label");
-    newLabel.textContent = "Новый label";
-    form.appendChild(newLabel);
-  });
+    var form = document.getElementById("myForm");
+    var labels =
+        form === null || form === void 0 ? void 0
+            : form.querySelectorAll("label");
+    labels.forEach(function (element, index) {
+        console.log(`Index ${index}: ${element.innerText}`);
+    })
+    form.addEventListener("submit", function (e) {
+        e.preventDefault();
+        var newLabel = document.createElement("label");
+        newLabel.textContent = "Новый label";
+        form.appendChild(newLabel);
+    });
 })();
